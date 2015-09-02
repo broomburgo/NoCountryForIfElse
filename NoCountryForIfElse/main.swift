547add73811b75bde86eeafa20b514e94e10d004
Elviro Rocca <elviro.rocca@facile.it>
2015-09-02 14:37:44 +0200

    Initial Commit

---
+++ NoCountryForIfElse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NoCountryForIfElse
+//
+//  Created by Elviro Rocca on 02/09/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
